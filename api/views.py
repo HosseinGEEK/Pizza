715adb8f9069fb33279d651304a1cbc907d69ea0
Hossein Asadi <hossein@emtiyaz.app>
2020-11-23 22:32:14 +0330

    modify

--- api/views.py
+++ api/views.py
@@ -393,7 +393,7 @@ def get_food_detail(request):
                 for i in list_peymaeysh:
                     _list.append(i.option_size.to_json())
                     fo_options.remove(i)
-                option_list.append(o.to_json(with_sizes=False, sizes_list=[len(_list),len(_list),]))
+                option_list.append(o.to_json(with_sizes=False, sizes_list=_list))
                 list_peymaeysh.clear()
                 _list.clear()
 
